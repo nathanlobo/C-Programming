0cc6e3c190f351c1850e59371ed8ff4f288dd455
Nathan Lobo <113709797+nathanlobo@users.noreply.github.com>
2022-11-21 13:09:51 +0530

    Update factorial_num.c

--- factorial_num.c
+++ factorial_num.c
@@ -1,19 +1,19 @@
+
+
 // Program to find factorial of a number
+
 #include <stdio.h>
+
 int main() 
 {
-    system("cls");
-    int n, i;
-    unsigned long long fact = 1;
+    
+    int num, i, fact=1;
     printf("Enter an integer to find it's factorial: ");
-    scanf("%d", &n);
-    if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.\n");
-    else {
-        for (i=1; i<= n; i++) {
+    scanf("%d", &num);
+        for (i=num; i>=1; i--) {
             fact*=i;
         }
-        printf("Factorial of %d = %llu\n", n, fact);
-    }
+        printf("Factorial of %d = %d\n", num, fact);
+    
     return 0;
 }
